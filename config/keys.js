cbabe98abb7a43a045712ab55f8dd9b35cd3fa6c
Kaasim Shaikh <53850980+kalishaikh@users.noreply.github.com>
2021-01-15 12:04:05 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://heroku_vhhgs419:Kaasimss4$@cluster1.a4ga6.mongodb.net/heroku_vhhgs419?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://heroku_vhhgs419:kaasimss4$@cluster1.a4ga6.mongodb.net/heroku_vhhgs419?retryWrites=true&w=majority'
     
 }
